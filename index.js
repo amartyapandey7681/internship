1cb9fd31f3e80e04cf0be13f41da6c8dde5e15da
Amartya pandey <41753884+amartyapandey7681@users.noreply.github.com>
2021-01-07 20:40:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+//organization name can be any string i have chosen adobe
 axios.get('https://api.github.com/orgs/adobe/repos')
   .then(response => {
     console.log(response.data);
