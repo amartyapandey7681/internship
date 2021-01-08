ab60fa324205c60acfc1302468403bd7e477ea01
Amartya pandey <41753884+amartyapandey7681@users.noreply.github.com>
2021-01-08 14:06:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+
+var express = require("express");
+var app= express();
+var bodyParser= require("body-parser");
 const axios = require('axios');
-//organization name can be any string i have chosen adobe
-axios.get('https://api.github.com/orgs/adobe/repos')
+
+var urlencodedParser = bodyParser.urlencoded({extended:false})
+app.use(express.static("public"));
+
+app.get('/',function(req,res){
+
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post('/give',urlencodedParser,function(req,res){
+ //  app.request('https://api.github.com/orgs/'+req.body.fname+'/repos', function(error,response,body){
+
+   // console.log(response.data);
+  //  })
+  axios.get('https://api.github.com/orgs/'+req.body.fname+'/repos')
   .then(response => {
     console.log(response.data);
     
@@ -8,3 +25,10 @@ axios.get('https://api.github.com/orgs/adobe/repos')
   .catch(error => {
     console.log(error);
   });
+    res.send("CHECK TERMINAL FOR REPO NAMES");
+    //console.log(req.body.fname);
+    
+});
+
+
+app.listen(3000);
