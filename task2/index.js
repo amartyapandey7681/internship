5ecc0827be63079276ebe892870f322ab801b8b5
amartyapandey7681 <amartyapandey@gmail.com>
2021-01-10 15:38:35 +0530

    task2

---
+++ task2/index.js
@@ -0,0 +1,66 @@
+var express= require("express");
+var fs=require("fs");
+var bp= require("body-parser");
+var app= express();
+var port=3000;
+
+let mysql= require("mysql");
+
+var connect = mysql.createConnection({
+
+  host: 'localhost',
+  user: 'amartya',
+  password:'amartya123@',
+  database:'names'
+});
+
+
+//serves the main login page to the user
+app.get('/', function(req,res){
+var html= fs.readFileSync(__dirname+"/login.html");
+res.write(html);
+});
+
+//serves the css of the login page
+app.get('/logincss.css', function(req,res){
+    res.sendFile(__dirname+"/logincss.css");
+    });
+
+//serves the css of the signup page
+app.get('/signincss.css', function(req,res){
+    res.sendFile(__dirname+"/signincss.css");
+    });
+
+//serves the css page of the signup portal
+app.get('/signin.html', function(req,res){
+    var html= fs.readFileSync(__dirname+"/signin.html");
+    res.write(html);
+});
+
+//post request of signup page
+app.use(bp.urlencoded({extended : true}));
+app.post('/signinn',function(req,res){
+
+   connect.connect(function(error){
+    
+    if(error)throw error;
+    console.log("connected");
+    let sql= 'INSERT INTO new_table(id,name) VALUES (100,"superman")';
+    connect.query(sql,function(err,result){
+      if(err)throw err;
+      console.log("1 record inserted");
+    });
+    });
+   
+  });
+
+
+//post request of the login page
+app.use(bp.urlencoded({extended : true}));
+app.post('/loginn',function(req,res){
+ 
+
+    
+});
+
+app.listen(port);
